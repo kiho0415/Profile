236a3ee7bc2ebe50d3ff681453cbe746a073e819
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-05-12 00:00:18 +0900

    プロフアプリ応用の完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -11,19 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileLabel: UILabel!
     @IBOutlet var profileCommentLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profileImageView.isHidden = true
-        profileCommentLabel.isHidden = true
         // Do any additional setup after loading the view.
     }
 
     @IBAction func tapBUtton1(){
-        profileImageView.isHidden = false
-        profileCommentLabel.isHidden = false
+        profileImageView.image = UIImage(named: "philImage")
+        profileLabel.text = "名前"
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
     }
+    @IBAction func tapButton2(){
+           profileImageView.image = UIImage(named: "trackImage")
+           profileLabel.text = "スポーツ"
+           profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+       }
+    @IBAction func tapButton3(){
+             profileImageView.image = UIImage(named: "appleImage")
+             profileLabel.text = "好きな食べもの"
+             profileCommentLabel.text = "リンゴが好きで、いつも持ち歩いている"
+         }
+    @IBAction func tapButton4(){
+             profileImageView.image = UIImage(named: "flightImage")
+             profileLabel.text = "趣味"
+             profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+         }
 }
 
